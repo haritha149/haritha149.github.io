23da3ed5cd5611e1c15b85dffa0d8ff711f92429
bitlab.haritha <haritha@bitlab.academy>
2018-11-14 15:00:01 +0530

    initial commit

---
+++ precache-manifest.cf7f304c742c0fed3c4a669c9ba19944.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8e2a77588d8fdd3373da50d7ef99976",
+    "url": "/static/media/image 2.c8e2a775.svg"
+  },
+  {
+    "revision": "a4b1681063541abbca3b",
+    "url": "/static/css/main.1eeb3dbb.chunk.css"
+  },
+  {
+    "revision": "e1bfb6d6943cb995e1a0",
+    "url": "/static/js/1.e1bfb6d6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48bb04c10847bb64d3fb18239d0400ae",
+    "url": "/static/media/logo6.48bb04c1.svg"
+  },
+  {
+    "revision": "50969e0abc3f233a42604a571ee501dc",
+    "url": "/static/media/Arrow.50969e0a.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "51925855a4f329977f6e06c12f77beca",
+    "url": "/static/media/logo1.51925855.svg"
+  },
+  {
+    "revision": "af2ec018ec73ecceaca7dd2c856def60",
+    "url": "/static/media/logo2.af2ec018.svg"
+  },
+  {
+    "revision": "bacad7999b3d90a428fb90a520f0f8cb",
+    "url": "/static/media/logo3.bacad799.svg"
+  },
+  {
+    "revision": "019ef43fc839baa07888c5cf7a6b6d16",
+    "url": "/static/media/logo4.019ef43f.svg"
+  },
+  {
+    "revision": "a4b1681063541abbca3b",
+    "url": "/static/js/main.a4b16810.chunk.js"
+  },
+  {
+    "revision": "c43037a169d69015f446c3184f32d194",
+    "url": "/static/media/image 3.c43037a1.svg"
+  },
+  {
+    "revision": "603eb19f5c53cdfb7c403fe33767de9d",
+    "url": "/static/media/image 4.603eb19f.svg"
+  },
+  {
+    "revision": "7784cd901afb5bd74025ee0278a424d1",
+    "url": "/static/media/image 5.7784cd90.svg"
+  },
+  {
+    "revision": "f7e7813be1815c6b8343e7a5299269b0",
+    "url": "/static/media/image 6.f7e7813b.svg"
+  },
+  {
+    "revision": "e0d8708ac0d20ab8550b8248575d851e",
+    "url": "/static/media/image 7.e0d8708a.svg"
+  },
+  {
+    "revision": "45db11379a5d9c8c8265a03c00ad15c4",
+    "url": "/static/media/logo5.45db1137.svg"
+  },
+  {
+    "revision": "7c575c04906592bb057f14406002dd68",
+    "url": "/static/media/logo7.7c575c04.svg"
+  },
+  {
+    "revision": "c3da04c4bb7e08074b0b105538a90ad7",
+    "url": "/static/media/logo8.c3da04c4.svg"
+  },
+  {
+    "revision": "1853a99dcceb4239b01fa14268a55d11",
+    "url": "/static/media/logo9.1853a99d.svg"
+  },
+  {
+    "revision": "a05692ebed29e2031f0e3d22ce94a957",
+    "url": "/static/media/logo10.a05692eb.svg"
+  },
+  {
+    "revision": "4e6c6d0eb285b67a801251647ff4729d",
+    "url": "/index.html"
+  }
+];
